35422b31017cd97f06836ce409f4898ee90aec80
u05606 <sofia.perez@grupocarreras.com>
2024-02-06 14:04:52 +0100

    fixes

--- src/main/java/ar/edu/iua/rest/EstimuloRestController.java
+++ src/main/java/ar/edu/iua/rest/EstimuloRestController.java
@@ -1,9 +1,13 @@
 package ar.edu.iua.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import ar.edu.iua.business.IEstimuloBusiness;
 import ar.edu.iua.business.IOrdenBusiness;
 import ar.edu.iua.business.exception.BusinessException;
+import ar.edu.iua.business.exception.NotFoundException;
+import ar.edu.iua.model.Documento;
 import ar.edu.iua.model.Estimulo;
 import ar.edu.iua.model.Orden;
 import ar.edu.iua.model.dto.MensajeRespuesta;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -36,7 +43,7 @@ public class EstimuloRestController {
 			@ApiResponse(code = 201, message = "estimulo creado"),
 			@ApiResponse(code = 400, message = "El servidor no procesará la solicitud porque no puede o no debe debido  a un error del usuario "),
 			@ApiResponse(code = 500, message = "Error interno del servidor") })
-	@PostMapping(value = "/nuevaOrden", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/nuevoEstimulo", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<MensajeRespuesta> load(@RequestBody Estimulo estimulo) {
 
 		try {
@@ -50,6 +57,41 @@ public class EstimuloRestController {
 			}
 		} catch (BusinessException e) {
 			return new ResponseEntity<MensajeRespuesta>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@ApiOperation(value = "Obtener listado de estimulos", response = Documento.class)
+
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Operación exitosa"),
+			@ApiResponse(code = 500, message = "Error interno del servidor") })
+
+	@GetMapping(value = "/estimulos", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Estimulo>> list() {
+		try {
+
+			return new ResponseEntity<List<Estimulo>>(estimuloBusiness.list(), HttpStatus.OK);
+
+		} catch (BusinessException e) {
+			return new ResponseEntity<List<Estimulo>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@ApiOperation(value = "Obtener una orden por numero de orden", response = Orden.class)
+
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Operación exitosa"),
+			@ApiResponse(code = 404, message = "Orden no encontrada"),
+			@ApiResponse(code = 500, message = "Error interno del servidor") })
+
+	@GetMapping(value = "/obtenerEstimulo/{idEstimulo}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Estimulo> load(
+			@ApiParam(value = "El numero del estimulo que se desea obtener") @PathVariable("idEstimulo") int idEstimulo) {
+
+		try {
+			return new ResponseEntity<Estimulo>(estimuloBusiness.load(idEstimulo), HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<Estimulo>(HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<Estimulo>(HttpStatus.NOT_FOUND);
 		}
 	}
 
